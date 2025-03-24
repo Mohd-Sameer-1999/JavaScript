9052851d2be1576fc479d4364ffbce99b7186b85
mohd-sameer-1999 <mohdsameer2006@gmail.com>
2025-03-24 15:14:04 +0530

    solved 2 more questions Strivers's A2Z DSA sheet

--- armstrong.js
+++ armstrong.js
@@ -1,3 +1,14 @@
-function solve(n) {
-    
+function solve(num) {
+    let temp = num.toString();
+    let n = temp.length;
+    let sum = 0;
+    for(let i = 0; i < n; i++){
+        sum += Math.pow(Number(temp[i]), n);
+    }
+    if(sum === num){
+        return true;
+    }
+    return false;
 }
+
+console.log(solve(103))
